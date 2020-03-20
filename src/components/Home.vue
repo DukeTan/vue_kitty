9f07535af32634800ce2e9d4d90acea95385c0ab
DukeTan <51882906+DukeTan@users.noreply.github.com>
2020-03-20 13:14:56 +0800

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,16 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">
-            Exit
-        </el-button>
-    </div>
+    <el-container>
+        <!--Header-->
+        <el-header>Header<el-button type="info" @click="logout">Exit</el-button>
+        </el-header>
+        <!--main area-->
+        <el-container>
+            <!--aside-->
+            <el-aside width="200px">Aside</el-aside>
+            <!--right content-->
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
