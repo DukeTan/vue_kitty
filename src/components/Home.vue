0bd6096229fc643772d899e504a7ae915259d5dc
DukeTan <51882906+DukeTan@users.noreply.github.com>
2020-03-20 21:52:12 +0800

    day4

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,46 +1,82 @@
 <template>
-    <el-container class="home-container">
-        <!--Header-->
-        <el-header>
-            <div>
-                <img src="" alt="">
-                <span>Kitty E-commerce Management System</span>
-            </div>
-            <el-button type="info" @click="logout">Exit</el-button>
-        </el-header>
-        <!--main area-->
-        <el-container>
-            <!--aside-->
-            <el-aside width="200px">Aside</el-aside>
-            <!--right content-->
-            <el-main>Main</el-main>
-        </el-container>
+  <el-container class="home-container">
+    <!--Header-->
+    <el-header>
+      <div>
+        <img src="../assets/main_logo.jpg" alt />
+        <span>Kitty E-commerce Management System</span>
+      </div>
+      <el-button type="info" @click="logout">Exit</el-button>
+    </el-header>
+    <!--main area-->
+    <el-container>
+      <!--aside-->
+      <el-aside width="200px">
+        <!--menu area-->
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+          <!--1st class menu-->
+          <el-submenu index="1">
+            <!--1st class menu template-->
+            <template slot="title">
+              <!--icon-->
+              <i class="el-icon-location"></i>
+              <!-- content -->
+              <span>Navigator1</span>
+            </template>
+            <!--2nd class menu-->
+            <el-menu-item index="1-4-1">
+              <!--2nd class menu template-->
+              <template slot="title">
+                <!--icon-->
+                <i class="el-icon-location"></i>
+                <!-- content -->
+                <span>content1</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!--right content-->
+      <el-main>Main</el-main>
     </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-    methods : {
-        logout(){
-            window.sessionStorage.clear();
-            this.$router.push('/login');
-        }
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
     }
-}
+  }
+};
 </script>
 
 <style lang="less" scoped>
-.home-container{
-    height: 100%;
+.home-container {
+  height: 100%;
 }
-.el-header{
-    background-color: #373D41;
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  padding-right: 0;
+  color: antiquewhite;
+  font-size: 20px;
+  > div {
     display: flex;
+    align-items: center;
+    span {
+      margin-left: 10px;
+    }
+  }
 }
-.el-aside{
-    background-color: #333744;
+.el-aside {
+  background-color: #333744;
 }
-.el-main{
-    background-color: #EAEDF1;
+.el-main {
+  background-color: #eaedf1;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from '../src/App'
 import router from './router'
-import { Button, FormItem, Form, Input, Message, Container, Aside, Header, Main } from 'element-ui'
+import { Button, FormItem, Form, Input, Message, Container, Aside, Header, Main, Menu, Submenu, MenuItem } from 'element-ui'
 
 import './assets/css/global.css'
 
@@ -11,8 +11,16 @@ import axios from 'axios'
 //set request root url
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
 
+//before mount to Vue prototype use interceptors to add authorization field to req head
+axios.interceptors.request.use((config) => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config 
+})
+
 //achieve global mount to Vue prototype
 Vue.prototype.$http = axios
+
 Vue.prototype.$message = Message
 
 Vue.use(Button)
@@ -23,6 +31,9 @@ Vue.use(Container)
 Vue.use(Header)
 Vue.use(Main)
 Vue.use(Aside)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
 
 Vue.config.productionTip = false
 
